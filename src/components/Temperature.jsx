efd724bca2296c50cdb01d76fb60339db5f65858
Brian Henning <henninb@msn.com>
2023-05-25 07:01:54 -0500

    wip

--- src/components/Temperature.jsx
+++ src/components/Temperature.jsx
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react';
+import { useEffect, useCallback } from 'react'; //useState
 import axios from 'axios';
 // import cheerio from 'cheerio';
 
